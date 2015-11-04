2e2ef7fd7b5327d99be056821c8abe93e8843be2
saasbook <saasbook@saasbook.(none)>
2015-11-04 05:53:08 -0400

    My blog destroy action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,15 +30,17 @@ class PostsController < ApplicationController
     	respond_to do |format|
 	      if @post.update_attributes(params[:post])
 	        format.html { redirect_to @post, notice: 'Blog was successfully updated.' }
-	        #format.json { head :no_content }
 	      else
 	        format.html { render action: "edit" }
-	        #format.json { render json: @blog.errors, status: :unprocessable_entity }
 	      end
 	    end
 	end
 
 	def destroy
-
+		@post = Post.find(params[:id])
+    	@post.destroy
+	    respond_to do |format|
+	      format.html { redirect_to posts_url }
+	    end
 	end
 end
